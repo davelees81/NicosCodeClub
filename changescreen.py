d42f7628180964c2b05c2ff4fbf2888f78b4da05
Mikelees19 <mike.lees@hotmail.co.uk>
2022-10-07 09:25:49 +0100

    Delete changescreen.py

--- changescreen.py
+++
@@ -1,5 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-# from kivy.uix.boxlayout import BoxLayout
-# from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
